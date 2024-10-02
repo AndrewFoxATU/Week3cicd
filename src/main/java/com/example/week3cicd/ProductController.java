a0a2f89d61ca66a606690201814bad8bd62d1cfb
AndrewFoxATU <G00417373@atu.ie>
2024-10-02 14:43:55 +0100

    Feature1

---
+++ src/main/java/com/example/week3cicd/ProductController.java
@@ -0,0 +1,30 @@
+package com.example.week3cicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.color.ProfileDataException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ProductController {
+    private List<Product> products= new ArrayList<>();
+    public ProductController() {
+        products.add (new Product("Tv", "Made by Sony", 899, 100));
+        products.add (new Product("Radio", "Made by Samsung", 99, 101));
+    }
+
+    @GetMapping("/getProducts")
+    public List<Product> getProducts()
+    {
+        return products;
+    }
+    @PostMapping("addProduct")
+    public ResponseEntity<List> addProduct(@RequestBody Product product)
+    {
+        products.add(product);
+        return ResponseEntity.ok(products);
+    }
+
+}
